934a70697ef35f2c1ced9464face537c2eadd697
Muhammed Moustafa <muhammed.atya@outlook.com>
2025-02-12 01:17:16 +0200

    lesson3

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+
+
+mongoose.connect('mongodb+srv://MuhammedQC:UdZzbV9..ivZL2k@cluster0.lnpqg.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+  });
+})
+.catch((err) => {
+  console.log('Error connecting to the database: ', err)
 })
